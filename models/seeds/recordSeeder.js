b1b0a932db4cc35c4648350f602e5217f72fe881
shibao <dream0184@yahoo.com.tw>
2021-11-09 18:12:34 +0800

    feat: add record seeder

---
+++ models/seeds/recordSeeder.js
@@ -0,0 +1,43 @@
+const db = require('../../config/mongoose')
+const Record = require('../record')
+
+const records = [
+  {
+    name: '早餐',
+    category: 'food',
+    date: '2021-11-9',
+    amount: '80'
+  },
+  {
+    name: '遊戲片',
+    category: 'entertainment',
+    date: '2021-11-9',
+    amount: '815'
+  },
+  {
+    name: '肥皂',
+    category: 'home-related',
+    date: '2021-11-9',
+    amount: '200'
+  },
+  {
+    name: '補習費',
+    category: 'other',
+    date: '2021-11-9',
+    amount: '5600'
+  },
+  {
+    name: '交通費',
+    category: 'transportation',
+    date: '2021-11-9',
+    amount:' 60'
+  }
+]
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+  for (let i = 0; i < records.length; i++) {
+    Record.create(records[i])
+  }
+  console.log('added restaurants to db')
+})
