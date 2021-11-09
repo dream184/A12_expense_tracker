79439393b6ba5a286aa950b3094e7c6410e5176d
shibao <dream0184@yahoo.com.tw>
2021-11-09 16:59:40 +0800

    feat: add new, edit, index pages

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 const port = 3000
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
-require('./config/mongoose')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/records/edit', (req, res) => {
+  res.render('edit')
+})
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
