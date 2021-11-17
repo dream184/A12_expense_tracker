875a729a1db274514103068404f6c87a85e76189
shibao <dream0184@yahoo.com.tw>
2021-11-18 00:21:04 +0800

    feat: install bcrypt and encypt user's password when storaging into DB

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
   app.use(passport.initialize())
@@ -13,11 +14,14 @@ module.exports = app => {
           console.log('找不到該使用者!')
           return done(null, false)
         }
-        if (user.password !== password) {
-          console.log('密碼不相符!')
-          return done(null, false)
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              console.log('密碼不相符!')
+              return done(null, false)
+            }
+            return done(null, user)
+          })      
       })
       .catch(error => done(error, false))
   }))
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -31,9 +32,15 @@ router.post('/register', (req, res) => {
           confirmPassword
         })
       } else {
-        User.create({ name, email, password })
+        return bcrypt.genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          })
           .then(() => res.redirect('/'))
-          .catch(err => console.log(err))
+          .catch(err => console.log(err)) )      
       }
     })
 })
