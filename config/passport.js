3dd10b8dd4986d7510fdae96731262589a3c4695
shibao <dream0184@yahoo.com.tw>
2021-11-17 21:32:43 +0800

    feat: add register user function

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,13 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('../models/user')
+
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ email })
+      .then
+  }))
+}
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -10,4 +10,25 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('這個 Email 已經註冊過了。')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        User.create({ name, email, password })
+          .then(() => res.redirect('/'))
+          .catch(err => console.log(err))
+      }
+    })
+})
+
 module.exports = router
